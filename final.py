b71a07dd4bdfeb2f1803a1e40f86470b84e32751
francescabeller <49878590+francescabeller@users.noreply.github.com>
2020-05-24 11:25:37 -0600

    Training videos, reports, and Python code

---
+++ final.py
@@ -0,0 +1,134 @@
+# Import packages
+import os
+os.environ['HDF5_DISABLE_VERSION_CHECK'] = '1'
+
+import cv2     # for capturing videos
+import math   # for mathematical operations
+import matplotlib.pyplot as plt    # for plotting the images
+%matplotlib inline
+import pandas as pd
+from keras.preprocessing import image   # for preprocessing the images
+import numpy as np    # for mathematical operations
+from keras.utils import np_utils
+from skimage.transform import resize   # for resizing images
+
+
+#####################################################################
+############ READ VIDEO, EXTRACT FRAMES, SAVE AS IMAGES #############
+#####################################################################
+
+cd = os.getcwd()
+os.mkdir(f'{cd}/frames')
+# Extract pass video frames
+for i in range(1,6):
+    count = 0
+    videoFile = f"{cd}\Videos\pass{i}.mp4"
+    cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
+    frameRate = cap.get(5) #frame rate
+    x=1
+    while(cap.isOpened()):
+        frameId = cap.get(1) #current frame number
+        ret, frame = cap.read()
+        if (ret != True):
+            break
+        if (frameId % math.floor(frameRate) == 0):
+            ext = f'pass{i}_'
+            filename =f"{cd}/frames/{ext}frame%d.jpg" % count;count+=1
+            cv2.imwrite(filename, frame)
+    cap.release()
+    print ("Done!")
+
+# Extract run video frames
+for i in range(1,6):
+    count = 0
+    videoFile = f"{cd}\Videos\\run{i}.mp4"
+    cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
+    frameRate = cap.get(cv2.CAP_PROP_FPS) #frame rate
+    x=1
+    while(cap.isOpened()):
+        frameId = cap.get(1) #current frame number
+        ret, frame = cap.read()
+        if (ret != True):
+            break
+        if (frameId % math.floor(frameRate) == 0):
+            ext = f'run{i}_'
+            filename =f"{cd}/frames/{ext}frame%d.jpg" % count;count+=1
+            cv2.imwrite(filename, frame)
+    cap.release()
+    print ("Done!")
+
+
+########################################
+############ IMAGE MAPPING #############
+########################################
+
+# Read in mapping.csv
+data = pd.read_csv(f'{cd}/mapping.csv')
+
+# Create array of image files
+X = [ ]     # creating an empty array
+for img_name in data.Image_ID:
+    img = plt.imread('' + img_name)
+    X.append(img)  # storing each image in array X
+X = np.array(X)    # converting list to array
+
+# Categorical encoding
+y = data.Class
+dummy_y = np_utils.to_categorical(y)    # one hot encoding Classes
+
+# Reshape images
+image = []
+for i in range(0,X.shape[0]):
+    a = resize(X[i], preserve_range=True, output_shape=(224,224)).astype(int)      # reshaping to 224*224*3
+    image.append(a)
+X = np.array(image)
+
+# Pre-processing
+from keras.applications.vgg16 import preprocess_input
+X = preprocess_input(X, mode='tf')
+
+
+######################################
+############ BUILD MODEL #############
+######################################
+
+# Split training and test
+from sklearn.model_selection import train_test_split
+X_train, X_valid, y_train, y_valid = train_test_split(X, dummy_y, test_size=0.3, random_state=42)
+
+# Import packages
+from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense, InputLayer, Dropout
+
+# Load base model
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3)) # include_top=False to remove the top layer
+
+# Make predictions, get features, retrain
+X_train = base_model.predict(X_train)
+X_valid = base_model.predict(X_valid)
+X_train.shape, X_valid.shape
+
+# Reshape to 1-D
+X_train = X_train.reshape(65, 7*7*512)
+X_valid = X_valid.reshape(29, 7*7*512)
+
+# Pre-process images, make zero-centered
+train = X_train/X_train.max()
+X_valid = X_valid/X_train.max()
+
+
+# i. Building the model
+model = Sequential()
+model.add(InputLayer((7*7*512,))) # input layer
+model.add(Dense(units=1024, activation='sigmoid')) # hidden layer
+model.add(Dense(2, activation='softmax')) # output layer
+
+# Model summary
+model.summary()
+
+# ii. Compiling the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# iii. Training the model
+model.fit(train, y_train, epochs=100, validation_data=(X_valid, y_valid))
