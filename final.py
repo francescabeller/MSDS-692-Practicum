adbf2fa024888eda8d68288e5e11c8a30f8f71f8
francescabeller <49878590+francescabeller@users.noreply.github.com>
2020-06-21 11:01:14 -0600

    Update videos, script

--- final.py
+++ final.py
@@ -111,8 +111,8 @@ X_valid = base_model.predict(X_valid)
 X_train.shape, X_valid.shape
 
 # Reshape to 1-D
-X_train = X_train.reshape(70, 7 * 7 * 512)
-X_valid = X_valid.reshape(24, 7 * 7 * 512)
+X_train = X_train.reshape(275, 7 * 7 * 512)
+X_valid = X_valid.reshape(92, 7 * 7 * 512)
 
 # Pre-process images, make zero-centered
 train = X_train / X_train.max()
@@ -213,7 +213,7 @@ for i in tqdm(range(test_videos.shape[0])):
 accuracy_score(predict, actual) * 100
 
 #######################################################################################################
-#######################################################################################################
+######################################## SECOND ITERATION #############################################
 #######################################################################################################
 
 
@@ -224,7 +224,7 @@ accuracy_score(predict, actual) * 100
 cd = os.getcwd()
 os.mkdir(f'{cd}/frames2')
 # Extract pass video frames
-for i in range(1, 16):
+for i in range(1, 21):
     count = 0
     videoFile = f"{cd}/training_videos2/pass{i}.mp4"
     cap = cv2.VideoCapture(videoFile)
@@ -233,7 +233,7 @@ for i in range(1, 16):
     while cap.isOpened():
         frameId = cap.get(1)
         ret, frame = cap.read()
-        if ret != True:
+        if not ret:
             break
         if frameId % math.floor(frameRate) == 0:
             ext = f'pass{i}_'
@@ -244,7 +244,7 @@ for i in range(1, 16):
     print("Done!")
 
 # Extract run video frames
-for i in range(1, 16):
+for i in range(1, 21):
     count = 0
     videoFile = f"{cd}/training_videos2/run{i}.mp4"
     cap = cv2.VideoCapture(videoFile)
@@ -253,7 +253,7 @@ for i in range(1, 16):
     while cap.isOpened():
         frameId = cap.get(1)
         ret, frame = cap.read()
-        if ret != True:
+        if not ret:
             break
         if frameId % math.floor(frameRate) == 0:
             ext = f'run{i}_'
@@ -308,8 +308,8 @@ X_valid = base_model.predict(X_valid)
 X_train.shape, X_valid.shape
 
 # Reshape to 1-D
-X_train = X_train.reshape(210, 7 * 7 * 512)
-X_valid = X_valid.reshape(71, 7 * 7 * 512)
+X_train = X_train.reshape(275, 7 * 7 * 512)
+X_valid = X_valid.reshape(92, 7 * 7 * 512)
 
 # Pre-process images, make zero-centered
 train = X_train / X_train.max()
@@ -385,6 +385,204 @@ for i in tqdm(range(test_videos.shape[0])):
 
     # Read frames from 'test_frames2' directory
     images = glob("test_frames2/*.jpg")
+
+    prediction_images = []
+    for i in range(len(images)):
+        img = keras.preprocessing.image.load_img(images[i], target_size=(224, 224, 3))
+        img = keras.preprocessing.image.img_to_array(img)
+        img = img / 255
+        prediction_images.append(img)
+
+    # converting all the frames for a test video into numpy array
+    prediction_images = np.array(prediction_images)
+    # extracting features using pre-trained model
+    prediction_images = base_model.predict(prediction_images)
+    # converting features in one dimensional array
+    prediction_images = prediction_images.reshape(prediction_images.shape[0], 7 * 7 * 512)
+    # predicting tags for each array
+    prediction = model.predict_classes(prediction_images)
+    # appending the mode of predictions in predict list to assign the tag to the video
+    predict.append(test_y.columns.values[s.mode(prediction)[0][0]])
+    # appending the actual tag of the video
+    # actual.append(video_file.split('/')[1].split('_')[1])
+
+# Get accuracy
+accuracy_score(predict, actual) * 100
+
+
+#######################################################################################################
+######################################### THIRD ITERATION #############################################
+#######################################################################################################
+
+
+#####################################################################
+############ READ VIDEO, EXTRACT FRAMES, SAVE AS IMAGES #############
+#####################################################################
+
+cd = os.getcwd()
+os.mkdir(f'{cd}/frames3')
+# Extract pass video frames
+for i in range(1, 26):
+    count = 0
+    videoFile = f"{cd}/training_videos3/pass{i}.mp4"
+    cap = cv2.VideoCapture(videoFile)
+    frameRate = cap.get(5)
+    x = 1
+    while cap.isOpened():
+        frameId = cap.get(1)
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if frameId % math.floor(frameRate) == 0:
+            ext = f'pass{i}_'
+            filename = f"{cd}/frames3/{ext}frame%d.jpg" % count
+            count += 1
+            cv2.imwrite(filename, frame)
+    cap.release()
+    print("Done!")
+
+# Extract run video frames
+for i in range(1, 26):
+    count = 0
+    videoFile = f"{cd}/training_videos3/run{i}.mp4"
+    cap = cv2.VideoCapture(videoFile)
+    frameRate = cap.get(cv2.CAP_PROP_FPS)
+    x = 1
+    while cap.isOpened():
+        frameId = cap.get(1)
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if frameId % math.floor(frameRate) == 0:
+            ext = f'run{i}_'
+            filename = f"{cd}/frames3/{ext}frame%d.jpg" % count
+            count += 1
+            cv2.imwrite(filename, frame)
+    cap.release()
+    print("Done!")
+
+########################################
+############ IMAGE MAPPING #############
+########################################
+
+# Read in mapping.csv
+data = pd.read_csv(f'{cd}/mapping3.csv')
+
+# Create array of image files
+X = []
+for img_name in data.Image_ID:
+    img = plt.imread('' + img_name)
+    X.append(img)
+X = np.array(X)
+
+# Categorical encoding
+y = data.Class
+dummy_y = np_utils.to_categorical(y)
+y = y.to_frame()
+
+# Reshape images
+image = []
+for i in range(0, X.shape[0]):
+    a = resize(X[i], preserve_range=True, output_shape=(224, 224)).astype(int)
+    image.append(a)
+X = np.array(image)
+
+# Pre-processing
+X = preprocess_input(X, mode='tf')
+
+######################################
+############ BUILD MODEL #############
+######################################
+
+# Split training and test
+X_train, X_valid, y_train, y_valid = train_test_split(X, dummy_y, test_size=0.25, random_state=42)
+
+# Load base model
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+# Make predictions, get features, retrain
+X_train = base_model.predict(X_train)
+X_valid = base_model.predict(X_valid)
+X_train.shape, X_valid.shape
+
+# Reshape to 1-D
+X_train = X_train.reshape(346, 7 * 7 * 512)
+X_valid = X_valid.reshape(116, 7 * 7 * 512)
+
+# Pre-process images, make zero-centered
+train = X_train / X_train.max()
+X_valid = X_valid / X_train.max()
+
+# Build the model
+model = Sequential()
+model.add(InputLayer((7 * 7 * 512,)))
+model.add(Dense(units=1024, activation='sigmoid'))
+model.add(Dense(2, activation='softmax'))
+
+# Model summary
+model.summary()
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(train, y_train, epochs=100, validation_data=(X_valid, y_valid))
+
+#####################################
+############ TEST MODEL #############
+#####################################
+
+# Get list of test file names
+test_file_path = cd + r'\test_videos3'
+f = open(test_file_path + '\\' + 'test_list.txt', 'r')
+temp = f.read()
+videos = temp.split('\n')
+
+# Create dataframe
+test = pd.DataFrame({'video_name': videos})
+test_videos = test['video_name']
+
+classes = []
+for index, row in test.iterrows():
+    if 'pass' in row['video_name']:
+        classes.append(1)
+    else:
+        classes.append(0)
+test['class'] = classes
+
+# creating the tags
+test_y = test['class']
+test_y = pd.get_dummies(test_y)
+
+# Create lists to store tags
+predict = []
+actual = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+
+# Create directory to hold test video frames
+os.mkdir(f'{cd}/test_frames3')
+
+# Loop to extract test video frames
+for i in tqdm(range(test_videos.shape[0])):
+    count = 0
+    video_file = test_videos[i]
+    cap = cv2.VideoCapture('test_videos3/' + video_file)
+    frameRate = cap.get(5)
+    x = 1
+
+    while cap.isOpened():
+        frameId = cap.get(1)
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if frameId % math.floor(frameRate) == 0:
+            # Store video frames in newly created 'test_frames2' directory
+            filename = 'test_frames3/' + video_file[:-4] + "_frame%d.jpg" % count
+            count += 1
+            cv2.imwrite(filename, frame)
+    cap.release()
+
+    # Read frames from 'test_frames2' directory
+    images = glob("test_frames3/*.jpg")
 
     prediction_images = []
     for i in range(len(images)):
